6362cb826355ce6775cb6d383a840dca96af9ffc
Guga Zimmermann <gugazimmermann@gmail.com>
2022-10-01 11:24:26 -0300

    remove conn test

--- src/pages/home/Home.tsx
+++ src/pages/home/Home.tsx
@@ -1,16 +1,5 @@
-import { useEffect } from 'react';
-import ProfileAPI from '../../api/profile';
 export default function Home() {
-
-  const handleGetAll = async () => {
-    const { data } = await ProfileAPI.getAll();
-    console.log(data)
-  }
-
-  useEffect(() => {
-    handleGetAll();
-  }, [])
-
+  
   return (
     <section>
       Home Page
