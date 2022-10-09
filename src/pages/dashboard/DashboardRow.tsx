ecf0127095d25f7db830a5d40782e38bcbb0b389
Guga Zimmermann <gugazimmermann@gmail.com>
2022-10-09 15:31:32 -0300

    refactor to events and subs

--- src/pages/dashboard/DashboardRow.tsx
+++ src/pages/dashboard/DashboardRow.tsx
@@ -1,50 +1,35 @@
 import {
-  useContext,
   useState,
   useEffect,
   ReactElement,
   useCallback,
 } from "react";
 import EventsAPI from "../../api/events";
-import { Grid, Loading, Title } from "../../components";
-import { AppContext } from "../../context";
+import { Grid, LoadingSmall } from "../../components";
 import { PLANSTYPES } from "../../interfaces/enums";
 import { EventType } from "../../interfaces/types";
 import DashboardCard from "./DashboardCard";
 
-type DashboardRowProps = {
-  type: PLANSTYPES;
-};
-
-export default function DashboardRow({
-  type,
-}: DashboardRowProps): ReactElement {
-  const { state } = useContext(AppContext);
+export default function DashboardRow(): ReactElement {
   const [loading, setLoading] = useState(false);
   const [showContent, setShowContent] = useState<EventType[]>([]);
 
   const orderContent = useCallback(async () => {
     setLoading(true);
-    if (state.profile) {
-      const content = await EventsAPI.getByProfileID();
-      setShowContent(content);
-    }
+    const content = await EventsAPI.getByProfileID();
+    setShowContent(content);
     setLoading(false);
-  }, [state.profile]);
+  }, []);
 
   useEffect(() => {
     orderContent();
   }, [orderContent]);
-
-  if (loading) return <Loading />;
+  if (loading) return <LoadingSmall />;
   return (
     <div>
-      <Title
-        text={type !== PLANSTYPES.SUBSCRIPTION ? "Eventos" : "Assinaturas"}
-      />
       {!showContent.length ? (
         <h1 className="font-bold text-lg text-center my-4">
-          'Nenhum registro encontrado!'
+          Nenhum registro encontrado!
         </h1>
       ) : (
         <Grid>
