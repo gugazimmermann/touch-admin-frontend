7ea0c1d1a6a7b2dfacdd8888692ee1da94c365f4
Guga Zimmermann <gugazimmermann@gmail.com>
2022-10-09 15:29:24 -0300

    fix dashboard

--- src/pages/home/Home.tsx
+++ src/pages/home/Home.tsx
@@ -1,10 +1,9 @@
-import { PLANSTYPES } from '../../interfaces/enums';
 import DashboardRow from '../dashboard/DashboardRow';
 
 export default function Dashboard() {
 	return (
 		<div className="grid gap-4">
-      <DashboardRow type={PLANSTYPES.ADVANCED} />
+      <DashboardRow />
 		</div>
 	);
 }
