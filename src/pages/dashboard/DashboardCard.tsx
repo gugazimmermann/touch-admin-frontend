75f9afc4851004ad699f8c37f47a72c681195344
Guga Zimmermann <gugazimmermann@gmail.com>
2022-10-10 03:44:37 -0300

    refactor card and rows

--- src/pages/dashboard/DashboardCard.tsx
+++ src/pages/dashboard/DashboardCard.tsx
@@ -1,38 +1,56 @@
-import { ReactElement } from 'react';
-import { Link } from 'react-router-dom';
-import { PLANSTYPES, ROUTES } from '../../interfaces/enums';
-import { EventType } from '../../interfaces/types';
+import { ReactElement } from "react";
+import { Link } from "react-router-dom";
+import { PLANSTYPES, ROUTES } from "../../interfaces/enums";
+import { EventType } from "../../interfaces/types";
+import { DateTime } from "luxon";
 
 const LOGO_MAPS_BUCKET = process.env.REACT_APP_LOGO_MAPS_BUCKET || "";
 
-
 type DashboardCardProps = {
-	type: PLANSTYPES;
-	content: EventType;
-}
+  content: EventType;
+};
 
-export default function DashboardCard({ type, content }: DashboardCardProps): ReactElement {
-	return (
-		<Link to={`${ROUTES.EVENTS}/${content.eventID}`} key={content.eventID} className="flex flex-col justify-between shadow-md rounded-lg">
-			<div className="w-full bg-gray-200 rounded-t-lg overflow-hidden">
-				{content.logo ? (
-					<img
-						src={`https://${LOGO_MAPS_BUCKET}.s3.amazonaws.com${content?.logo}`}
-						alt={content.name}
-						className="w-full h-full object-center object-cover group-hover:opacity-75"
-					/>
-				) : (
-					<img
-						src="/image-placeholder.png"
-						alt={content.name}
-						className="w-full h-full object-center object-cover opacity-10  group-hover:opacity-5"
-					/>
-				)}
-			</div>
-			<div className="p-4 text-center">
-				<h3 className="font-semibold">{content.name}</h3>
-				<p className="text-sm">{`${content.city} - ${content.state}`}</p>
-			</div>
-		</Link>
-	);
+export default function DashboardCard({
+  content,
+}: DashboardCardProps): ReactElement {
+  return (
+    <Link
+      to={`${ROUTES.EVENTS}/${content.eventID}`}
+      key={content.eventID}
+      className={`flex flex-col justify-between shadow-md rounded-lg ${
+        content.planType === PLANSTYPES.ADVANCED
+          ? "bg-orange-50"
+          : content.planType === PLANSTYPES.BASIC
+          ? "bg-emerald-50"
+          : "bg-sky-50"
+      }`}
+    >
+      <div className="w-full bg-gray-200 rounded-t-lg overflow-hidden">
+        {content.logo ? (
+          <img
+            src={`https://${LOGO_MAPS_BUCKET}.s3.amazonaws.com${content?.logo}`}
+            alt={content.name}
+            className="w-full h-full object-center object-cover group-hover:opacity-75"
+          />
+        ) : (
+          <img
+            src="/image-placeholder.png"
+            alt={content.name}
+            className="w-full h-full object-center object-cover opacity-10  group-hover:opacity-5"
+          />
+        )}
+      </div>
+      <div className="p-2 py-4 text-center">
+        <h3 className="font-semibold">{content.name}</h3>
+        <p className="text-sm">{`${content.city} - ${content.state}`}</p>
+        <p className="text-sm">
+          {content.dates
+            .map((d) =>
+              DateTime.fromFormat(d, "yyyy-MM-dd").toFormat("dd/MM/yy")
+            )
+            .join(" | ")}
+        </p>
+      </div>
+    </Link>
+  );
 }
--- src/pages/dashboard/DashboardRow.tsx
+++ src/pages/dashboard/DashboardRow.tsx
@@ -1,11 +1,7 @@
-import {
-  useState,
-  useEffect,
-  ReactElement,
-  useCallback,
-} from "react";
+import { DateTime } from "luxon";
+import { useState, useEffect, ReactElement, useCallback } from "react";
 import EventsAPI from "../../api/events";
-import { Grid, LoadingSmall } from "../../components";
+import { Form, Grid, LoadingSmall, Title } from "../../components";
 import { PLANSTYPES } from "../../interfaces/enums";
 import { EventType } from "../../interfaces/types";
 import DashboardCard from "./DashboardCard";
@@ -13,37 +9,83 @@ import DashboardCard from "./DashboardCard";
 export default function DashboardRow(): ReactElement {
   const [loading, setLoading] = useState(false);
   const [showContent, setShowContent] = useState<EventType[]>([]);
+  const [currentEvents, setCurrentEvents] = useState<EventType[]>([]);
+  const [pastEvents, setPastEvents] = useState<EventType[]>([]);
+  const [futureEvents, setFutureEvents] = useState<EventType[]>([]);
+  const [subscriptions, setSubscriptions] = useState<EventType[]>([]);
+
+  const seeCurrentEvents = (e: EventType) => {
+    if (e.dates.find((d) => d === DateTime.now().toFormat("yyyy-MM-dd")))
+      return e;
+  };
+
+  const seePastEvents = useCallback((e: EventType) => {
+    if (
+      e.dates.find((d) => DateTime.fromFormat(d, "yyyy-MM-dd") > DateTime.now())
+    ) {
+      if (!seeCurrentEvents(e)) return e;
+    }
+  }, []);
+
+  const seeFutureEvents = useCallback((e: EventType) => {
+    if (
+      e.dates.find((d) => DateTime.fromFormat(d, "yyyy-MM-dd") < DateTime.now())
+    ) {
+      if (!seeCurrentEvents(e)) return e;
+    }
+  }, []);
 
   const orderContent = useCallback(async () => {
     setLoading(true);
     const content = await EventsAPI.getByProfileID();
+    const events = content.filter(
+      (e) => e.planType !== PLANSTYPES.SUBSCRIPTION
+    );
+    const subs = content.filter((e) => e.planType === PLANSTYPES.SUBSCRIPTION);
+    setSubscriptions(subs);
+    const current = events.map((e) => seeCurrentEvents(e)).filter((x) => x);
+    setCurrentEvents(current as EventType[]);
+    const furure = events.map((e) => seePastEvents(e)).filter((x) => x);
+    setFutureEvents(furure as EventType[]);
+    const past = events.map((e) => seeFutureEvents(e)).filter((x) => x);
+    setPastEvents(past as EventType[]);
     setShowContent(content);
     setLoading(false);
-  }, []);
+  }, [seeFutureEvents, seePastEvents]);
 
   useEffect(() => {
     orderContent();
   }, [orderContent]);
+
+  const renderRow = (title: string, events: EventType[]) => (
+    <>
+      <Title text={title} className="font-bold text-center -mb-4" />
+      <Form>
+        <Grid>
+          <>
+            {events.map((e) => <DashboardCard key={e.eventID} content={e}  />)}
+          </>
+        </Grid>
+      </Form>
+    </>
+  );
+
   if (loading) return <LoadingSmall />;
+
   return (
-    <div>
+    <>
       {!showContent.length ? (
         <h1 className="font-bold text-lg text-center my-4">
           Nenhum registro encontrado!
         </h1>
       ) : (
-        <Grid>
-          <>
-            {showContent.map((content) => (
-              <DashboardCard
-                key={content.eventID}
-                type={PLANSTYPES.ADVANCED}
-                content={content}
-              />
-            ))}
-          </>
-        </Grid>
+        <>
+          {!!currentEvents.length && renderRow('Eventos Acontecendo', currentEvents)}
+          {!!subscriptions.length && renderRow('Asstinaturas', subscriptions)}
+          {!!futureEvents.length && renderRow('Eventos Programados', futureEvents)}
+          {!!pastEvents.length && renderRow('Eventos Passados', pastEvents)}
+        </>
       )}
-    </div>
+    </>
   );
 }
