a9f607d7c54eada6129b53c3aca9a1f20ea3654c
Guga Zimmermann <gugazimmermann@gmail.com>
2022-10-14 13:07:31 -0300

    fix sms price

--- src/helpers/smsPrice.tsx
+++ src/helpers/smsPrice.tsx
@@ -5,8 +5,8 @@ const SMS_PRICE = process.env.REACT_APP_SMS_PRICE || 0.02297;
 const smsPrice = async () => {
   const { data } = await axios.get('https://economia.awesomeapi.com.br/last/USD-BRL');
   const dolar = data.USDBRL.bid;
-  const price = (Math.round(((dolar * (+SMS_PRICE)) + Number.EPSILON) * 100) / 100) + 0.01
-  return price;
+  const price = (Math.round(((dolar * (+SMS_PRICE)) + Number.EPSILON) * 100) / 100) + 0.02
+  return +price.toFixed(2);
 }
 
 export default smsPrice;
