012d50aadc97d7b69bf8e2bfafebe83c202ace81
Guga Zimmermann <gugazimmermann@gmail.com>
2022-10-14 13:07:22 -0300

    add getObjKey

--- src/helpers/general.ts
+++ src/helpers/general.ts
@@ -2,5 +2,6 @@ import { ContextStateType } from "../interfaces/context";
 import { ALERT } from "../interfaces/enums";
 
 export const showLink = (state: ContextStateType): boolean =>
-  !state.alerts.filter((a) => a.type === ALERT.WARNING)
-  .length;
+  !state.alerts.filter((a) => a.type === ALERT.WARNING).length;
+
+export const getObjKey = (obj: any, value: string) => Object.keys(obj).find(key => obj[key] === value)
