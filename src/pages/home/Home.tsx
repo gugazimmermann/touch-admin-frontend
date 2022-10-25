b7fac576dcd01d3706827ed4d0b359497455dd15
Guga Zimmermann <gugazimmermann@gmail.com>
2022-10-25 13:46:35 -0300

    navigate to alert

--- src/pages/home/Home.tsx
+++ src/pages/home/Home.tsx
@@ -1,6 +1,17 @@
+import { useContext, useEffect } from 'react';
+import { useNavigate } from 'react-router-dom';
+import { AppContext } from '../../context';
+import { ROUTES } from '../../interfaces/enums';
 import DashboardRow from '../dashboard/DashboardRow';
 
 export default function Dashboard() {
+	const navigate = useNavigate();
+	const { state } = useContext(AppContext);
+
+	useEffect(() => {
+		if (state.alerts.length) navigate(ROUTES.ALERTS)
+	}, [navigate, state.alerts.length]);
+
 	return (
 		<div className="grid gap-4">
       <DashboardRow />
